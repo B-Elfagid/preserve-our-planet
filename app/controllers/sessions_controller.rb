e9209ad214dd859d28903fb9121517b21a0d3462
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-02-08 22:42:44 +0000

    amend login & logout html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,20 @@ class SessionsController < ApplicationController
 
   # POST: /sessions
   post "/login" do
-    
+    user = User.find_by_email(params["user"]["email"])
+    if user && user.authenticate(params["user"]["password"])
+      flash[:message] = "Successfully logged-in"
+      redirect "/actions"
+    else
+      flash[:error] = "Unable to log in, please check your details"
+      redirect "/login"
   end
+end 
 
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end 
  
 end
