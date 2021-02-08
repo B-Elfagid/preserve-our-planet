c76d9a09b73ed3055a76d5614f3a17499f83a307
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-02-08 00:07:15 +0000

    add session_secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get "/" do
-    binding.pry
     erb :welcome
   end
 
